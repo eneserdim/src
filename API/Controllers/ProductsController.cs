d3a5691fc1c376ab6e0dddf75863b364538e8604
Enes Erdim <eneserdim1@gmail.com>
2021-12-23 05:41:49 +0300

    Tekil Product Metodundan Asenkron özelliğini kaldırdım

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -28,9 +28,9 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")] // http verbs -> get post delete put
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public ActionResult<Product> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return  _context.Products.Find(id);
         }
 
     }
